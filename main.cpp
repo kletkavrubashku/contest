19455567a120a3c1b3c6ce789e28359bba51accc
Artem Selishchev <arselishev@gmail.com>
2018-08-19 20:08:19 +0300

    fix indents

--- main.cpp
+++ main.cpp
@@ -40,10 +40,10 @@ templXYZ Z& operator<<(Z& str, const pair<X,Y>& p)  { str << p.fi << p.se; re st
 // sep_ostream -------------------------------------------------------------------
 struct sep { sep(string sep) : _(sep) {} string _; };
 struct sep_ostream {
-  sep_ostream(ostream& str, string sep) : _str(str), _sep(sep) {}
-  templX sep_ostream& operator<<(const X& a) { if (_step-- > 1) {*this << a;} else _str << (_step ? _sep : "") << a; re *this; }
-  sep_ostream& operator<<(ostream& (*manip)(ostream&)) { manip(_str); re *this; }
- private: ostream& _str; string _sep; int _step = 2;
+    sep_ostream(ostream& str, string sep) : _str(str), _sep(sep) {}
+    templX sep_ostream& operator<<(const X& a) { if (_step-- > 1) {*this << a;} else _str << (_step ? _sep : "") << a; re *this; }
+    sep_ostream& operator<<(ostream& (*manip)(ostream&)) { manip(_str); re *this; }
+private: ostream& _str; string _sep; int _step = 2;
 };
 sep_ostream operator<<(ostream& str, sep s) { re sep_ostream(str, s._); }
 // logging -----------------------------------------------------------------------
@@ -55,22 +55,22 @@ sep_ostream operator<<(ostream& str, sep s) { re sep_ostream(str, s._); }
 #define IF_NOT_DEBUG(A) A
 #endif
 struct debug_stream {
-  debug_stream(sep_ostream str) : _str(str) { IF_DEBUG( str << "\033[34m"; ) }
-  ~debug_stream() { IF_DEBUG( _str << "\033[0m" << endl; ) }
-  templX debug_stream& operator<<(const X& a) { IF_DEBUG( _str << a; ) re *this; }
-  templX debug_stream& batch(const X& a) { IF_DEBUG( *this << a; ) re *this; }
-  templXYs debug_stream& batch(const X& a, const Ys&... b) { IF_DEBUG( *this << a; batch(b...); ) re *this; }
-  debug_stream& operator<<(ostream& (*manip)(ostream&)) { IF_DEBUG( _str << manip; ) re *this; }
- private: sep_ostream _str;
+    debug_stream(sep_ostream str) : _str(str) { IF_DEBUG( str << "\033[34m"; ) }
+    ~debug_stream() { IF_DEBUG( _str << "\033[0m" << endl; ) }
+    templX debug_stream& operator<<(const X& a) { IF_DEBUG( _str << a; ) re *this; }
+    templX debug_stream& batch(const X& a) { IF_DEBUG( *this << a; ) re *this; }
+    templXYs debug_stream& batch(const X& a, const Ys&... b) { IF_DEBUG( *this << a; batch(b...); ) re *this; }
+    debug_stream& operator<<(ostream& (*manip)(ostream&)) { IF_DEBUG( _str << manip; ) re *this; }
+private: sep_ostream _str;
 };
 #define dout debug_stream(sep_ostream(cout, "\t"))
 #define doutX(...) IF_DEBUG( dout.batch(#__VA_ARGS__" =", __VA_ARGS__) );
 // time --------------------------------------------------------------------------
 using namespace chrono;
 struct debug_time {
-  debug_time(sep_ostream str) : _str(str), start(system_clock::now()) {}
-  ~debug_time() { IF_DEBUG( _str << "\n\033[31m" << "Elapsed time = " << duration_cast<milliseconds>(system_clock::now()-start).count() << "\033[0m"; ) }
- private: sep_ostream _str; time_point<system_clock> start;
+    debug_time(sep_ostream str) : _str(str), start(system_clock::now()) {}
+    ~debug_time() { IF_DEBUG( _str << "\n\033[31m" << "Elapsed time = " << duration_cast<milliseconds>(system_clock::now()-start).count() << "\033[0m"; ) }
+private: sep_ostream _str; time_point<system_clock> start;
 };
 #define TIMER debug_time l(sep_ostream(cout, ""));
 #define SLEEP IF_DEBUG( this_thread::sleep_for(chrono::milliseconds(2)) );
@@ -80,8 +80,8 @@ struct debug_time {
 
 int main()
 {
-  IF_NOT_DEBUG( ios_base::sync_with_stdio(false) );
-  IF_NOT_DEBUG( cin.tie(nullptr) );
-  //IF_NOT_DEBUG( freopen("input.txt", "r", stdin); )
-  //IF_NOT_DEBUG( freopen("out.txt", "w", stdout); )
+    IF_NOT_DEBUG( ios_base::sync_with_stdio(false) );
+    IF_NOT_DEBUG( cin.tie(nullptr) );
+    //IF_NOT_DEBUG( freopen("input.txt", "r", stdin); )
+    //IF_NOT_DEBUG( freopen("out.txt", "w", stdout); )
 }
